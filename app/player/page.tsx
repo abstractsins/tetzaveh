1c234a20451cf95cb2758b760e76f96cf81884f6
Daniel Berlin <daniel.berlin@comcast.net>
2025-10-28 00:56:00 -0400

    whatever

--- app/player/page.tsx
+++ app/player/page.tsx
@@ -2,8 +2,6 @@
 
 import MediaPlayer from '@/component/player/MediaPlayer';
 import styles from './page.module.css';
-import TrackList from '@/component/player/TrackList';
-import PlaylistSelect from '@/component/player/PlaylistSelect';
 import MediaLibrary from '@/component/player/MediaLibrary';
 
 
